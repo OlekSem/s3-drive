4db9d4cb659b7ea5bb7c654caf0ca412ad6aa630
Ostapchuk Vlad <ostapchukvladislav77@gmail.com>
2026-05-23 15:18:58 +0300

    add navbar labels

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -1,4 +1,9 @@
 import Logo from "../ui/Logo.tsx";
+import {LuFileStack} from "react-icons/lu";
+import Label from "../ui/Label.tsx";
+import {FaTrashCan} from "react-icons/fa6";
+import {FaFolderMinus, FaRegFileImage} from "react-icons/fa";
+import {IoDocument} from "react-icons/io5";
 
 
 
@@ -6,16 +11,24 @@ const Navbar = () => {
     return (
         <div
             className="flex flex-col
-            justify-between
+            justify-start
             bg-[var(--surface)]
             w-[25%] p-6 border-r-2 border-[var(--border)]
             h-screen
             ">
             <Logo />
-            {/*<div*/}
-            {/*    className="mt-6 flex flex-col gap-4">*/}
+            <div
+                className="mt-10 flex flex-col gap-4">
+                <Label title={"All files"} icon={<LuFileStack />} />
+                <Label title={"Bin"} icon={<FaTrashCan />} />
+            </div>
+            <div
+                className="mt-10 flex flex-col gap-4">
+                <Label title={"Images"} icon={<FaRegFileImage />} />
+                <Label title={"Documents"} icon={<IoDocument />} />
+                <Label title={"Folders"} icon={<FaFolderMinus />} />
+            </div>
 
-            {/*</div>*/}
         </div>
     );
 };
